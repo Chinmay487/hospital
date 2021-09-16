0b8157aa9ef705b298bdf205814a196da892885c
Chinmay487 <pchinmay04@gmail.com>
2021-09-16 09:29:19 +0530

    updated urls

--- staff/urls.py
+++ staff/urls.py
@@ -8,7 +8,5 @@ urlpatterns = [
     path('logout',views.Logout_Page.as_view(),name="logout"),
     path('new_patient',New_patient.as_view(),name="new_patient"),
     path('details/',Details.as_view(),name="details"),
-    path('update/update',Update_info.as_view(),name='update_info'),
     path('update/',Update_info.as_view(),name='update_info')
-    # path(r'^update/(?P<patient_id>\d+)/$',Update_info.as_view(),name='update_info')
 ]
